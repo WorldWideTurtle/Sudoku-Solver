62cd099c016b9b4d3218147e9848540317a3d089
WorldWideTurtle <148323035+WorldWideTurtle@users.noreply.github.com>
2025-01-16 18:27:15 +0100

    Add validation function for sudoku boards

--- src/lib/util/sudokuUtil.ts
+++ src/lib/util/sudokuUtil.ts
@@ -13,5 +13,50 @@ export const sudokuUtil = {
 
     encode(arr : number[][]) {
         return arr.reduce((row,nextRow)=>row.concat(nextRow.reduce<string>((p,c)=>p+=c.toString(),""),""),"");
+    },
+
+    isValid(str: string) {
+        const arr = this.decode(str);
+
+        for (let r = 0; r < 9; r++) {
+            let rowSet = new Set<number>()
+            let colSet = new Set<number>()
+            let squareSet = new Set<number>()
+
+            let row = arr[r];
+            for (let c = 0; c < 9; c++) {
+                const rowNumber = row[c]
+                const colNumber = arr[c][r]
+
+                const squareRow = (~~(r / 3)) * 3 + (~~(c / 3));
+                const squareNumber = arr[squareRow][c % 3];
+
+                if (rowNumber !== 0) {
+                    if (rowSet.has(rowNumber)) {
+                        return false;
+                    } else {
+                        rowSet.add(rowNumber)
+                    }
+                };
+
+                if (colNumber !== 0) {
+                    if (colSet.has(colNumber)) {
+                        return false;
+                    } else {
+                        colSet.add(colNumber)
+                    }
+                }
+
+                if (squareNumber !== 0) {
+                    if (squareSet.has(squareNumber)) {
+                        return false;
+                    } else {
+                        squareSet.add(squareNumber)
+                    }
+                }
+            }
+        }
+
+        return true;
     }
 }
